c1b4a88cd1fb2b3d4b281e08ab08c22c4212659c
lbarbisan <lbarbisan@df68296a-424c-48ce-8dab-7561b5598764>
2008-11-09 08:02:23 +0000

    Beta 1: Impression fonctionnelle

--- trunk/src/fr/free/hd/servers/gui/FaceGenerator.java
+++ trunk/src/fr/free/hd/servers/gui/FaceGenerator.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.awt.image.FilteredImageSource;
@@ -28,17 +29,47 @@ public class FaceGenerator {
 
 	private static Map<String, Image> cachedImage = new HashMap<String, Image>();
 	private static Map<Phonem, Image> cachedFinalImage = new HashMap<Phonem, Image>();
-	private static Dimension dimension  = new Dimension(248, 350);
+	
+	
+	private static int mouthRatio = 200;
+	private static int handRatio = 30;
+	
+	private static int mouthYPercent = 680;
+	private static int mouthXPercent = 485;
+	private static int initialHeightSize = 350;
+	
+	private static int boucheX = 400;
+	private static int boucheY = 600;
+	private static int coteX = 0;
+	private static int coteY = 400;
+	private static int couX = 400;
+	private static int couY = 800;
+	private static int mentonX = 500;
+	private static int mentonY = 700;
+	private static int paumetteX = 300;
+	private static int paumetteY = 500;
+	
+	
+	private static Dimension dimension  = null;
 	
     public static Image Create(Phonem phonem) {
+    	
     	if(!cachedFinalImage.containsKey(phonem))
     	{
+	    	//Get Face
+	    	Image face = getFace();
+	    	
+	    	//Create final image
+	    	int width = initialHeightSize* face.getWidth(null) / face.getHeight(null);
+	    	dimension = new Dimension(width, initialHeightSize);
     		BufferedImage finalImage = new BufferedImage(dimension.width, dimension.height, BufferedImage.TYPE_INT_ARGB);
 	    	Graphics2D g2 = (Graphics2D)finalImage.createGraphics();
-	    	Image mouth = getMouth(phonem);
-	    	Image face = getFace();
+	    	
+	    	//Draw Face
 	    	g2.drawImage(face, 0, 0, dimension.width, dimension.height, null);
-	    	g2.drawImage(mouth,(dimension.width - mouth.getWidth(null))/2, 70*dimension.height/100 - mouth.getHeight(null)/2, null);
+	    	//DrawMouth
+	    	drawMouth(g2, phonem);
+	    	//Draw Hand
 	        drawHand(g2, dimension.width, dimension.height, phonem);
 	        cachedFinalImage.put(phonem, finalImage);
     	}
@@ -71,29 +102,34 @@ public class FaceGenerator {
     	switch(phonem.getHandPosition())
     	{
     	case HAND_POSITION_BOUCHE:
-            g.translate(90,240);
+    		g.translate(dimension.width*boucheX /1000,
+    					dimension.height*boucheY /1000);
             g.rotate(Math.toRadians(90));
     		break;
     	case HAND_POSITION_COTE:
-    		g.translate(0,180);
+    		g.translate(dimension.width*coteX /1000,
+						dimension.height*coteY /1000);
     		break;
     	case HAND_POSITION_COU:
-            g.translate(100,310);
+            g.translate(dimension.width*couX /1000,
+						dimension.height*couY /1000);
             g.rotate(Math.toRadians(90));
     		break;
     	case HAND_POSITION_MENTON:
-            g.translate(110,280);
+            g.translate(dimension.width*mentonX /1000,
+						dimension.height*mentonY /1000);
             g.rotate(Math.toRadians(90));
     		break;
     	case HAND_POSITION_PAUMETTE:
-    		g.translate(50,225);
+    		g.translate(dimension.width*paumetteX /1000,
+						dimension.height*paumetteY /1000);
     		g.rotate(Math.toRadians(60));
     		break;
     	}
         g.drawImage(hand, 0, 0, null);
     }
     
-    private static Image getMouth(Phonem phonem)
+    private static void drawMouth(Graphics2D g, Phonem phonem)
     {
     	String mouthImagePath = "mouth/" + phonem.getMouthVowel().toString() + ".png";
     	Image mouth = null;
@@ -101,13 +137,15 @@ public class FaceGenerator {
     	{
     		try {
     			mouth = ImageIO.read(LPCDraw.class.getResource(mouthImagePath));
+    			
+    			int width = mouthRatio*dimension.width/1000;
+    			int height = width * mouth.getHeight(null)/ mouth.getWidth(null);
+    			mouth = getScaledImage(mouth, width, height);
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 			
-            //mouth = ImageResizeHelper.resize(mouth, dimension.height/4, dimension.width/4);
-            //ImageResizeHelper.resize(mouth, dimension.height/4, dimension.width/4)
             mouth = makeColorTransparent(mouth, Color.GREEN);
             cachedImage.put(mouthImagePath, mouth);
        	
@@ -117,7 +155,8 @@ public class FaceGenerator {
     		mouth = cachedImage.get(mouthImagePath);
     	}
         
-        return mouth;  
+    	g.drawImage(mouth,	mouthXPercent*dimension.width/1000 - mouth.getWidth(null)/2,
+    						mouthYPercent*dimension.height/1000 - mouth.getHeight(null)/2, null);
     }
     
     private static Image getFace()
@@ -160,5 +199,22 @@ public class FaceGenerator {
 
     ImageProducer ip = new FilteredImageSource(im.getSource(), filter);
     return Toolkit.getDefaultToolkit().createImage(ip);
+    }
+    
+    
+    /**
+     * Resizes an image using a Graphics2D object backed by a BufferedImage.
+     * @param srcImg - source image to scale
+     * @param w - desired width
+     * @param h - desired height
+     * @return - the new resized image
+     */
+    private static Image getScaledImage(Image srcImg, int w, int h){
+        BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2 = resizedImg.createGraphics();
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.drawImage(srcImg, 0, 0, w, h, null);
+        g2.dispose();
+        return resizedImg;
     }
 }
--- trunk/src/fr/free/hd/servers/gui/PrintCommand.java
+++ trunk/src/fr/free/hd/servers/gui/PrintCommand.java
@@ -1,22 +1,27 @@
 package fr.free.hd.servers.gui;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.design.JasperDesign;
-import net.sf.jasperreports.engine.xml.JRXmlLoader;
-import net.sf.jasperreports.view.JasperViewer;
+import java.awt.Color;
+import java.awt.Image;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import org.springframework.richclient.command.ActionCommand;
 
-import com.sun.corba.se.impl.interceptors.PICurrent;
+import com.lowagie.text.BadElementException;
+import com.lowagie.text.Cell;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Table;
+import com.lowagie.text.pdf.PdfCell;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
 
-import fr.free.hd.servers.LPCDraw;
+import fr.free.hd.servers.entities.Phonem;
 
 public class PrintCommand extends ActionCommand {
 
@@ -27,24 +32,42 @@ public class PrintCommand extends ActionCommand {
 		  super("printCommand");
 	}
 	
+
+	/**
+	 * @see org.springframework.richclient.command.ActionCommand#doExecuteCommand()
+	 */
 	@Override
 	protected void doExecuteCommand() {
 
+		// etape 1: creation du document
+        Document document = new Document(PageSize.A4.rotate());
+        
+        // Create temp file.
+        File temp;
 		try {
-			// - Chargement et compilation du rapport
-			JasperDesign jasperDesign = JRXmlLoader.load(LPCDraw.class.getResourceAsStream("gui\\Simple.jrxml"));
-	
-	        JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
-	
-	        // - Paramètres à envoyer au rapport
-	        Map parameters = new HashMap();
-	        parameters.put("Titre", "Titre");
-	
-	        // - Execution du rapport
-	        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new PhonemDataSource(model));
-	        // - Création du rapport au format PDF
-	        JasperViewer.viewReport(jasperPrint);
-		} catch (JRException e) {
+			temp = File.createTempFile("lpcdraw", ".pdf");
+			// Delete temp file when program exits.
+			temp.deleteOnExit();
+            // etape 2:
+            // creation du writer -> PDF ou HTML 
+            PdfWriter.getInstance(document, new FileOutputStream(temp));
+            // etape 3: ouverture du document
+            document.open();
+            // etape 4: Creation d'un tableau  
+            PdfPTable table = createTable(model);	            
+            document.add(table);
+        
+        	document.close();
+            
+			Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + temp.getAbsolutePath());
+            
+        }
+        catch(DocumentException de) {
+            System.err.println(de.getMessage());
+        } catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -52,5 +75,34 @@ public class PrintCommand extends ActionCommand {
 
 	public void setModel(PhonenListModel model) {
 		this.model = model;
+	}
+	
+	
+	/*Création du tableau de notre emploi du temps*/	
+	public PdfPTable createTable(PhonenListModel model) throws BadElementException {
+		
+		PdfPTable table = new PdfPTable(model.getSize() > 6 ? 6 : model.getSize());
+		table.setSplitRows(false);
+		table.setWidthPercentage(model.getSize() > 6 ? 100 : model.getSize()*16);
+		table.getDefaultCell().setFixedHeight(160);
+		
+		for(int index = 0;index < model.getSize();index++)
+		{
+			PdfPTable innertable = new PdfPTable(1);
+			innertable.getDefaultCell().setFixedHeight(140);
+			Phonem phonem = (Phonem)model.getElementAt(index);
+			try {
+				innertable.addCell(com.lowagie.text.Image.getInstance( FaceGenerator.Create(phonem), Color.WHITE));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			PdfPCell cell = new PdfPCell(new Paragraph(phonem.getPhonem()));
+			cell.setFixedHeight(10);
+			innertable.addCell(cell);
+			table.addCell(innertable);
+		}
+		table.completeRow();
+		return table;
 	}
 }
