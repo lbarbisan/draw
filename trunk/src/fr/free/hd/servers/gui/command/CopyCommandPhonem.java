16d67e43d9543ad60e1cf763accd417dcf0ae2af
lbarbisan <lbarbisan@df68296a-424c-48ce-8dab-7561b5598764>
2009-01-01 15:00:37 +0000

    UPDATE : Remise en forme des image

--- trunk/src/fr/free/hd/servers/gui/command/CopyCommandPhonem.java
+++ trunk/src/fr/free/hd/servers/gui/command/CopyCommandPhonem.java
@@ -6,9 +6,16 @@ import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.image.BufferedImage;
 
+import javax.swing.ImageIcon;
+import javax.swing.SwingWorker;
+
+import org.springframework.richclient.application.Application;
+import org.springframework.richclient.application.ApplicationWindow;
 import org.springframework.richclient.command.ActionCommand;
+import org.springframework.richclient.progress.ProgressMonitor;
 
 import fr.free.hd.servers.entities.Face;
+import fr.free.hd.servers.entities.MouthVowelEnum;
 import fr.free.hd.servers.entities.Phonem;
 import fr.free.hd.servers.gui.StatementListModel;
 import fr.free.hd.servers.gui.tools.FaceGeneratorHelper;
@@ -22,25 +29,48 @@ public class CopyCommandPhonem extends ActionCommand {
 	@Override
 	protected void doExecuteCommand() {
 		
-		int size = model.getSize()* FaceGeneratorHelper.initialWidthSize;
-		BufferedImage finalImage = null;
-		Graphics2D g2 = null;
-		
-		for(int index = 0;index < model.getSize(); index++)
-		{
-			Phonem phonem = (Phonem) model.getElementAt(index);
-			Image image = FaceGeneratorHelper.Create(phonem, face, null);
-			if(finalImage==null)
-			{
-				finalImage = new BufferedImage(size, image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-				g2 = (Graphics2D)finalImage.createGraphics();
-			}
-			g2.drawImage(image, index*FaceGeneratorHelper.initialWidthSize, 0, null);
-		}
-		
-		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		Clipboard clipboard = toolkit.getSystemClipboard();
-		clipboard.setContents(new ImageSelection(finalImage), null);
+		ApplicationWindow aw = Application.instance().getActiveWindow();  
+		final ProgressMonitor pm = aw.getStatusBar().getProgressMonitor();
+		pm.taskStarted("Copying...", -1);  
+		SwingWorker sw = new SwingWorker() {  
+		            //protected Object construct() in SpringRC's SwingWorker!  
+		            protected Object doInBackground() {
+		            	int total = 0;
+		            	int size = model.getSize()* FaceGeneratorHelper.initialWidthSize;
+		            	int diff = 100/size;
+		        		BufferedImage finalImage = null;
+		        		Graphics2D g2 = null;
+		        		
+		        		for(int index = 0;index < model.getSize(); index++)
+		        		{
+		        			Phonem phonem = (Phonem) model.getElementAt(index);
+		        			Image image = FaceGeneratorHelper.Create(phonem, face, null);
+		        			if(finalImage==null)
+		        			{
+		        				finalImage = new BufferedImage(size, image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+		        				g2 = (Graphics2D)finalImage.createGraphics();
+		        			}
+		        			g2.drawImage(image, index*FaceGeneratorHelper.initialWidthSize, 0, null);
+		        			total+=diff;
+		        			pm.worked(total);
+		        		}
+		        		
+		        		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		        		Clipboard clipboard = toolkit.getSystemClipboard();
+		        		clipboard.setContents(new ImageSelection(finalImage), null);
+		        		
+		            	return null;
+		           }  
+		           //protected void finished()  
+		           @Override  
+		           protected void done() {  
+		               // all of the following code will be called on the Event Dispatching Thread  
+		               pm.done();  
+		           }  
+		       };  
+		       //sw.start();  
+		       //sw.clear(); // reuse would be possible with SpringRC's SwingWorker!  
+		       sw.execute();
 	}
 
 	public void setFace(Face face) {
