f5915094cc299ab8d99ccbdda347583552be81bd
lbarbisan <lbarbisan@df68296a-424c-48ce-8dab-7561b5598764>
2010-11-12 22:19:20 +0000

    1.0.5 : Mise à jour de déploiement

--- trunk/src/fr/free/hd/servers/gui/command/HelpCommand.java
+++ trunk/src/fr/free/hd/servers/gui/command/HelpCommand.java
@@ -1,15 +1,21 @@
 package fr.free.hd.servers.gui.command;
 
 import java.awt.Desktop;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.swing.JOptionPane;
 
 import org.springframework.richclient.command.ActionCommand;
 
-public class HelpCommand extends ActionCommand {
+import fr.free.hd.servers.LPCDraw;
 
+public class HelpCommand extends ActionCommand {
 
 	/**
 	 * @see org.springframework.richclient.command.ActionCommand#doExecuteCommand()
@@ -18,33 +24,54 @@ public class HelpCommand extends ActionCommand {
 	protected void doExecuteCommand() {
 
 		try {
-			
-			//Depends of
-			String filename = "Prise_en_main_logiciel.pdf";
-			File file = new File(filename);
-			if(!file.exists())
-			{
-				filename = ".." + File.separator  + filename;
-				file = new File(filename);
-				file = file.getCanonicalFile();
-			}
-			
-			Desktop.getDesktop().open(file);
-			
+
+			File temp = File.createTempFile("Prise_en_main_logiciel", ".pdf");
+			temp.deleteOnExit();
+
+			FileOutputStream writer = new FileOutputStream(temp);
+			InputStream stream = LPCDraw.class
+					.getResourceAsStream("help/Prise_en_main_logiciel.pdf");
+
+			copy(stream, writer);
+
+			Desktop.getDesktop().open(temp);
+
 		} catch (IOException e) {
 			showErrorMessage(e.getLocalizedMessage());
 		}
 	}
-	
-	protected static void showErrorMessage(String error)
-	{
+
+	protected static void copy(InputStream inFile, OutputStream outFile)
+			throws IOException {
+		InputStream in = null;
+		OutputStream out = null;
+		try {
+			in = new BufferedInputStream(inFile);
+			out = new BufferedOutputStream(outFile);
+			while (true) {
+				int data = in.read();
+				if (data == -1) {
+					break;
+				}
+				out.write(data);
+			}
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+			if (out != null) {
+				out.close();
+			}
+		}
+	}
+
+	protected static void showErrorMessage(String error) {
 		JOptionPane
-		.showMessageDialog(
-				null,
-				"Impossible d'ouvrir l'aide. Il est nécessaire d'avoir Acrobat Reader pour que l'aide s'affiche. \n"
-						+ "Pour télécharger Acrobat Reader : http://get.adobe.com/fr/reader/\n"
-						+ error,
-				"Impossible d'ouvrir l'aide",
-				JOptionPane.ERROR_MESSAGE);
+				.showMessageDialog(
+						null,
+						"Impossible d'ouvrir l'aide. Il est nécessaire d'avoir Acrobat Reader pour que l'aide s'affiche. \n"
+								+ "Pour télécharger Acrobat Reader : http://get.adobe.com/fr/reader/\n"
+								+ error, "Impossible d'ouvrir l'aide",
+						JOptionPane.ERROR_MESSAGE);
 	}
 }
